7d0725152f91c5be771123a6912f7868fb92c15d
mattasupriya24 <mattasupriya24@gmail.com>
2026-08-21 20:10:20 +0530

    added power and changed int to double

--- sum.c
+++ sum.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
-    int a,b;
+    double a,b,c;
     printf("enter two numbers\n");
-    scanf("%d %d",&a,&b);
-    printf("%d %c %d = %d",a,'+',b,a+b);
+    scanf("%lf %lf",&a,&b);
+    c=pow(a,b);
+    printf("%.2lf ^ %.2lf = %.2lf",a,b,c);
     return 0;
 }
