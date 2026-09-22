f36fd9a5a6c7bb68132f0950fe5c05437d689e26
mattasupriya24 <mattasupriya24@gmail.com>
2026-09-22 09:43:41 +0530

    square of given num

---
+++ square.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int num, square;
+    printf("enter a num:");
+    scanf("%d" ,&num);
+    square =num*num;
+    printf("square = %d\n" ,square);
+    return 0;
+}
